3c9e4273a845949d3ab0c9ff3d54c7339a27124e
ESPITY <132686177+ESPITY@users.noreply.github.com>
2025-12-13 18:08:09 +0100

    HTML export v2

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 @onready var death_screen = preload("res://Scenes/ui_scenes/death_screen.tscn")
 @onready var win_screen = preload("res://Scenes/ui_scenes/win_screen.tscn")
+@onready var explosion_vfx = preload("res://Scenes/explosion_vfx.tscn")
 
 var screen_size: Vector2
 var level_start_time: float = 0.0
@@ -43,7 +44,6 @@ func start_game():
 	player.global_position = screen_size / 2
 	
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	preload("res://Scenes/explosion_vfx.tscn")
 
 # Crea la región de navegación que ocupa toda la pantalla (+ margen)
 func set_nav_region():
